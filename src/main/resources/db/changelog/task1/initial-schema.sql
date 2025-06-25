3e88f5865c00315cbe03a793753af07482427f4a
Артем Мовчан <mob.ar@mail.ru>
2025-06-25 20:58:57 +0400

    Decimal на numeric заменить

--- src/main/resources/db/changelog/task1/initial-schema.sql
+++ src/main/resources/db/changelog/task1/initial-schema.sql
@@ -4,8 +4,8 @@ CREATE TABLE products (
     article VARCHAR(255) NOT NULL UNIQUE,
     description TEXT,
     category VARCHAR(255) NOT NULL,
-    price DECIMAL(19,2) NOT NULL,
-    quantity DECIMAL(19,2) NOT NULL,
+    price NUMERIC(19,2) NOT NULL,
+    quantity NUMERIC(19,2) NOT NULL,
     last_quantity_change TIMESTAMP WITH TIME ZONE NOT NULL,
     created_at TIMESTAMP NOT NULL
     );
